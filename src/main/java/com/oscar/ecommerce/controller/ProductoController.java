13e45d02e1efd92b77ea67e9bc14a24863735c7c
oscar <203241029@alumnos.utn.edu.mx>
2022-06-28 17:43:17 -0500

    se creo la funcionalidad para eliminar un registro producto

--- src/main/java/com/oscar/ecommerce/controller/ProductoController.java
+++ src/main/java/com/oscar/ecommerce/controller/ProductoController.java
@@ -70,5 +70,10 @@ public class ProductoController {
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		productoService.delete(id);
+		return "redirect:/productos";
+	}
 
 }
