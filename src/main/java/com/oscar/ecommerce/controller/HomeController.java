3f25a97ce00c31b6f49e8bbc918731ad9aa156ef
oscar <203241029@alumnos.utn.edu.mx>
2022-06-29 14:32:31 -0500

    SE creo la direccion del carrito de compras

--- src/main/java/com/oscar/ecommerce/controller/HomeController.java
+++ src/main/java/com/oscar/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.oscar.ecommerce.model.Producto;
@@ -45,6 +46,11 @@ public class HomeController {
 		model.addAttribute("producto", producto);
 		return "usuario/productohome";
 	}
-	
+	//carrito
+	@PostMapping("/cart")
+	public String addCart() {
+		
+		return "usuario/carrito";
+	}
 
 }
