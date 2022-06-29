c010b9195f615a5056542f9266f7e6c26b53595a
oscar <203241029@alumnos.utn.edu.mx>
2022-06-29 13:54:35 -0500

    Se mostro la vista de producto al usuario

--- src/main/java/com/oscar/ecommerce/controller/HomeController.java
+++ src/main/java/com/oscar/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.oscar.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.oscar.ecommerce.service.ProductoService;
@@ -11,6 +14,7 @@ import com.oscar.ecommerce.service.ProductoService;
 @Controller
 @RequestMapping("/")//aqui va aputar a la raiz del proyecto
 public class HomeController {
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
 	//private se refiere a nivel de clase
 	@Autowired
@@ -23,6 +27,14 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		
 		return"usuario/home";
+	}
+	
+	//El siguiente metodo tiene como funcion es llevarnos ver producto
+               //Le colocamos id para buscarlo por su id del producto	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("id producto enviado como parametro {}", id);
+		return "usuario/productohome";
 	}
 	
 
