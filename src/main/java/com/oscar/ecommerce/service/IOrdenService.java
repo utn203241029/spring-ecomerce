59c83ca367c0c9fdd3cbb479ac3574ac98a3a4e4
oscar <203241029@alumnos.utn.edu.mx>
2022-07-05 18:33:58 -0500

    Metodo para generar el numero de la orden

--- src/main/java/com/oscar/ecommerce/service/IOrdenService.java
+++ src/main/java/com/oscar/ecommerce/service/IOrdenService.java
@@ -1,8 +1,13 @@
 package com.oscar.ecommerce.service;
 
+import java.util.List;
+
 import com.oscar.ecommerce.model.Orden;
 
 public interface IOrdenService {
+	
+	//Va a permitir una lista de ordenes
+	List<Orden> findAll();
 	
 	Orden save(Orden orden);
 
--- src/main/java/com/oscar/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/oscar/ecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.oscar.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,49 @@ public class OrdenServiceImpl implements IOrdenService{
 	public Orden save(Orden orden) {
 		// TODO Auto-generated method stub
 		return OrdenRepository.save(orden);
+	}
+
+	@Override
+	public List<Orden> findAll() {
+		// Con esto optenemos todas las ordenes
+		return OrdenRepository.findAll();
+	}
+	
+	public String generarNumeroOrden() {
+		int numero=0;
+		String numeroConcatenado="";
+		
+		//findAll() para que nos retorne todas las ordenes
+		List<Orden> ordenes= findAll();
+		
+		//Aqui colocaremos la lista ordenes porque es una cadena string y la pasaremos a numeros
+		List<Integer> numeros=new ArrayList<Integer>();
+		
+		
+		ordenes.stream().forEach(p -> numeros.add(Integer.parseInt(p.getNumero())));
+		
+		if (ordenes.isEmpty()) {
+			numero=1;
+		}else {
+			numero=numeros.stream().max(Integer::compare).get();
+		    numero++;
+		}
+		
+		if (numero<10) {//000000001
+			numeroConcatenado="000000000"+String.valueOf(numero);
+			
+		}else if(numero<100){//000000100
+			numeroConcatenado="00000000"+String.valueOf(numero);
+			
+		}else if(numero<1000){//000001000
+			numeroConcatenado="0000000"+String.valueOf(numero);
+			
+		}else if(numero<10000){//000010000
+			numeroConcatenado="0000000"+String.valueOf(numero);
+			
+		}
+		
+		return numeroConcatenado;
 	}
 
 }
